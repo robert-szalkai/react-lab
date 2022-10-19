a2a07bbc1335731823c130545e9c3819d9af4371
Robert Szalkai <robert.szalkai.work@gmail.com>
2022-10-19 14:02:28 +0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Typography>Doctari React Labs</Typography>
+      </header>
+
+      <main className='main'>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
